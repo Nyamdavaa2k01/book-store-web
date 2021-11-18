eeeb9881fc55c2d6fbdcb6394b9b93946233d1d0
Nyamdavaa2k01 <nyamdavaa2k01@gmail.com>
2021-11-18 21:11:06 +0800

    Update main.js

--- Script codes/main.js
+++ Script codes/main.js
@@ -1,26 +1,29 @@
 import {News, getInfo} from './exporting.js'; 
 
             // export const url = "https://api.jsonbin.io/b/618c95b4820eda3cc81b7988/latest" ;    
-            const url = "https://api.jsonbin.io/b/61963b900ddbee6f8b0e09df/latest" ;
-            getInfo(url, 
-                (books) => {
-                    let i = 1 ; 
-                    books.forEach (
-                      (element) => {
-                          let id = 'book'+i; 
-                      document.getElementById(id).innerHTML += element.create() ; 
-                      // creating four clones is only temporary solution!!! 
-                            let id2 = 'book' + (10+i) ; 
-                            document.getElementById(id2).innerHTML += element.create() ; 
-                            let id3 = 'book' + (20+i) ; 
-                            document.getElementById(id3).innerHTML += element.create() ; 
-                            let id4 = 'book' + (30+i) ; 
-                            document.getElementById(id4).innerHTML += element.create() ; 
-                            // let id5 = 'book' + (40+i) ; 
-                            // document.getElementById(id5).innerHTML += element.create() ; 
-                      i ++ ; 
-                    })
-                    
-                })
+            export const url = "https://api.jsonbin.io/b/61963b900ddbee6f8b0e09df/latest" ;
+            setInterval(
+
+                getInfo(url, 
+                    (books) => {
+                        let i = 1 ; 
+                        books.forEach (
+                          (element) => {
+                              let id = 'book'+i; 
+                          document.getElementById(id).innerHTML += element.create() ; 
+                          // creating four clones is only temporary solution!!! 
+                                let id2 = 'book' + (10+i) ; 
+                                document.getElementById(id2).innerHTML += element.create() ; 
+                                let id3 = 'book' + (20+i) ; 
+                                document.getElementById(id3).innerHTML += element.create() ; 
+                                let id4 = 'book' + (30+i) ; 
+                                document.getElementById(id4).innerHTML += element.create() ; 
+                                // let id5 = 'book' + (40+i) ; 
+                                // document.getElementById(id5).innerHTML += element.create() ; 
+                          i ++ ; 
+                        })
+                        
+                    }), 3000
+            )
                 
             

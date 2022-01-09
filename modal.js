317bb06fccc817a41f52894b4fede71c0912927e
Nyamdavaa2k01 <nyamdavaa2k01@gmail.com>
2022-01-09 17:38:26 +0800

    pop up menu for mobile version

--- modal.js
+++ modal.js
@@ -1,5 +1,6 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]') ; 
 const closeModalButtons = document.querySelectorAll('[data-close-button]') ;
+
 const overlay = document.getElementById('overlay') ; 
 console.log(overlay);
 openModalButtons.forEach(button => {
